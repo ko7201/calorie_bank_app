241e4ff3b5fe889b56b38f5dcf1eddacddd953e9
ko7201 <ym.ko.ki2465@gmail.com>
2026-02-05 01:09:22 +0900

    lintエラーを修正

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  #トップページのルーティング
+  # トップページのルーティング
   root "static_pages#top"
 
   # ログイン後のトップページのルーティング
