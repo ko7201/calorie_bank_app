432df4f26abf3c5f80d18d3ec5c582084f79601e
ko7201 <ym.ko.ki2465@gmail.com>
2026-02-14 19:52:22 +0900

    プロフィール登録画面の実装

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,15 @@
 class ProfilesController < ApplicationController
+    before_action :authenticate_user!
+    # プロフィールの重複作成を防ぐ
+    before_action :redirect_if_profile_exists, only: [:new, :create]
+    before_action :hide_nav, only: [:new, :create]
+
     def new
         @profile = Profile.new
     end
 
     def create
-        @profile = Profile.new(profile_params)
-        @profile.user = current_user
+        @profile = current_user.build_profile(profile_params)
         if @profile.save
             redirect_to user_root_path, notice: "プロフィールが作成されました。"
         else
@@ -14,7 +18,16 @@ class ProfilesController < ApplicationController
     end
 
     private
+    # プロフィールの重複作成を防ぐためのリダイレクト
+    def redirect_if_profile_exists
+        redirect_to edit_profile_path(current_user.profile) if current_user.profile.present?
+    end
+
     def profile_params
         params.require(:profile).permit(:birth_date, :height, :weight, :activity_level, :weight_to_lose, :gender, :target_saving_calories)
     end
+
+    def hide_nav
+        @hide_nav = true
+    end 
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,16 @@
 class Profile < ApplicationRecord
     belongs_to :user
+
+    enum gender: { male: 0, female: 1, other: 2 }
+    enum activity_level: { low: 0, middle: 1, high: 2 }
+
+    ACTIVITY_NUMBER = {
+        "low" => 1.5,
+        "middle" => 1.25,
+        "high" => 2.0
+    }.freeze
+
+    def activity_number
+        ACTIVITY_NUMBER.fetch[activity_level]
+    end 
 end
