bffbcea1e901ba03c389d76c9ac0a551e7bfae28
ko7201 <ym.ko.ki2465@gmail.com>
2026-02-21 16:56:49 +0900

    feat: プロフィールにご飯基準値のカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_02_21_052636) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_21_075016) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_21_052636) do
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rice_bowls", default: 0, null: false
+    t.integer "rice_kcal_per_bowl", default: 0, null: false
     t.index ["user_id"], name: "index_calorie_records_on_user_id"
   end
 
@@ -38,6 +40,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_21_052636) do
     t.datetime "updated_at", null: false
     t.integer "age"
     t.integer "rice_gram"
+    t.integer "rice_kcal_per_bowl", default: 0, null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
