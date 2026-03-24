daa2d6adb7ef62acb2cb250ceac2be1c0c393cf1
ko7201 <ym.ko.ki2465@gmail.com>
2026-03-24 20:44:39 +0900

    feat: UserモデルにGoogle認証用のfrom_omniauthメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,13 @@ class User < ApplicationRecord
 
   has_one :profile, dependent: :destroy
   has_many :calorie_records, dependent: :destroy
+
+  # Googleから受け取った情報でログイン又はユーザー作成
+  def self.from_omniauth(auth)
+    find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name
+    end
+  end
 end
