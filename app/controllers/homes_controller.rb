c999aea79aecb30ad3dece5d78d5b2dfe78b0fe7
ko7201 <ym.ko.ki2465@gmail.com>
2026-04-15 16:28:47 +0900

    feat:ログイン後画面のカロリーカードUIを実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,15 @@
 class HomesController < ApplicationController
     before_action :authenticate_user!
 
+      MEALS = {
+    "朝食" => :breakfast,
+    "昼食" => :lunch,
+    "夕食" => :dinner,
+    "おやつ" => :snack
+  }.freeze
+
     def index
+        @meals = MEALS
         @calorie_records = current_user.calorie_records.today_total
     end
 end
