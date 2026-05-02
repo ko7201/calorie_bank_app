88d2ce04945f8b75a61cb5614c6096a9bcc7ab0a
ko7201 <ym.ko.ki2465@gmail.com>
2026-05-02 21:09:17 +0900

    refactor: ホーム画面用のカロリー集計処理を移動

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -11,5 +11,25 @@ class HomesController < ApplicationController
     def index
         @meals = MEALS
         @calorie_records = current_user.calorie_records.today_total
+        set_home_stats
     end
+
+    private
+
+  def set_home_stats
+    @today_total = current_user.calorie_records.today.sum(:calorie)
+    # プロフィール情報がない場合は0を表示する
+    profile = current_user.profile
+    unless profile
+      @calorie_goal = 0
+      @bmr = 0
+      @calorie_saved = 0
+      return
+    end
+
+    @calorie_goal = current_user.profile.target_saving_calories
+    @bmr = current_user.profile.bmr.round
+    @calorie_saved = [ @bmr - @today_total, 0 ].max
+    @rice_kcal_per_bowl = current_user.profile.rice_kcal_per_bowl
+  end
 end
