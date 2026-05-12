33ce7fb237e55898a7dfa3716a0ea80b8292eeb1
ko7201 <ym.ko.ki2465@gmail.com>
2026-05-12 22:49:28 +0900

    fix: lintエラーを解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: "users/sessions",
-    omniauth_callbacks: "users/omniauth_callbacks",
+    omniauth_callbacks: "users/omniauth_callbacks"
   }
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
