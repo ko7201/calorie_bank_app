90a4e2db31b2c57a2609678a8579825e4d60e627
ko7201 <ym.ko.ki2465@gmail.com>
2026-05-12 22:45:02 +0900

    fix: 未使用のDevise passwordsルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: "users/sessions",
     omniauth_callbacks: "users/omniauth_callbacks",
-    passwords: "users/passwords"
   }
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
