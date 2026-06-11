a84f2067e5ac2b25a1741a3063efbc20c7d0ad80
ko7201 <ym.ko.ki2465@gmail.com>
2026-06-11 13:29:33 +0900

    chore: 独自ドメインの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << "caloriebank.jp"
+  config.hosts << "www.caloriebank.jp"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
