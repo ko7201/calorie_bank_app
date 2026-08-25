8e12388e25f1571ec52db702963e9c6da1376805
ko7201 <ym.ko.ki2465@gmail.com>
2026-08-25 13:35:02 +0900

    test/ ログイン前ページの画面遷移テストコードを追記

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -6,4 +6,20 @@ RSpec.describe "StaticPages", type: :system do
     click_link "利用規約"
     expect(page).to have_content("利用規約")
   end
+
+  it "トップページからプライバシーポリシーページに遷移できる" do
+    visit root_path
+    click_link "プライバシーポリシー"
+    expect(page).to have_content("プライバシーポリシー")
+  end
+
+  #PC表示ページでのテスト
+  it "トップページから初めての方へページに遷移できる" do
+    visit root_path
+    click_link "初めての方へ", visible: true
+    expect(page).to have_content("初めての方へ")
+  end
+
+
 end
+
