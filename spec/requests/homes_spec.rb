b31ef1ae1884c939a12526c609f1ab72c5c580b1
ko7201 <ym.ko.ki2465@gmail.com>
2026-09-18 10:30:20 +0900

    test: homes_spec.rbのRequest specを追加

--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { User.create!(email: "test2@example.com", password: "password") }
+
+  before do
+    user.create_profile!(
+      age: 30,
+      height: 170,
+      weight: 60,
+      activity_level: :low,
+      weight_to_lose: 3,
+      gender: :male,
+      target_saving_calories: 300,
+      rice_gram: 150
+    )
+    sign_in user
+  end
+
+  it "returns http success" do
+    get homes_path
+    expect(response).to have_http_status(:success)
   end
 end
