410c691879e4e56a77586bc45832f0d4e96fd757
ko7201 <ym.ko.ki2465@gmail.com>
2026-09-18 09:36:49 +0900

    test: 利用規約・プライバシーポリシー・初めての方へページのRequest specを追加

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,4 +7,25 @@ RSpec.describe "StaticPages", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "GET /terms" do
+    it "正確にレスポンスが返ってくる" do
+      get terms_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET /privacy" do
+    it "正確にレスポンスが返ってくる" do
+      get privacy_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET /about" do
+    it "正確にレスポンスが返ってくる" do
+      get about_path
+      expect(response).to have_http_status(200)
+    end
+  end
 end
